6df1645f58614842803930a2c6ca9763c032fcde
Gabriel Arocha <143841331+gxbrielarocha@users.noreply.github.com>
2023-10-30 15:43:15 -0400

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import pandas as pd
 import requests
+import re
 
 # !/usr/bin/python
 
@@ -19,9 +19,10 @@ def Home():
 
     # Load and display your logo image with container width
 
-    # what i do
+# what i do
 
     with (st.container()):
+
 
         st.write("---")
 
@@ -69,7 +70,7 @@ def Recipes():
 
     if submit_button:
         if not user_query:
-            st.error("Please enter a query.")
+            st.error("Please enter an ingredient.")
         else:
             response = get_recipes(user_query)
             if response.status_code == 200:
@@ -78,24 +79,81 @@ def Recipes():
                 for recipe in recipes["results"]:
                     st.subheader(recipe["title"])  # Display recipe name
                     st.image(recipe["image"])  # Display recipe image
+                    st.write("Meal ID:", recipe["id"])  # Display meal ID
             else:
                 st.error(f"API request failed. Status code: {response.status_code}")
 
 
 
 
+def strip_html_tags(text):
+    # Use regular expressions to remove HTML tags and entities
+    clean_text = re.sub(r'<.*?>', '', text)
+    return clean_text
+
+def Ingredients():
+    with st.container():
+        # Add the title and description within the container
+        logo_image = "https://i.imgur.com/QtiDzrN.png"  # Replace with the actual path to your logo image
+        st.image(logo_image, width=100)
+
+    user_query = st.text_input("Enter Meal ID")
+
+    # Button to trigger the API request
+    submit_button = st.button("Search")
+
+    # Define your Spoonacular API key (replace with your actual key)
+    apiKey = "71648f5810894c3d905128dc3a497553"
+
+    # Define the API endpoint URL
+    api_url = "https://api.spoonacular.com/recipes/{id}/information"
+
+    # Function to make the API request
+    def get_recipe_information(recipe_id):
+        params = {
+            "apiKey": apiKey,
+        }
+        response = requests.get(api_url.format(id=recipe_id), params=params)
+        return response
+
+    if submit_button:
+        if not user_query:
+            st.error("Please enter a Meal ID.")
+        else:
+            response = get_recipe_information(user_query)
+            if response.status_code == 200:
+                recipe_info = response.json()
+                st.title("Recipe Information for Meal ID " + user_query)
+                st.write("Title:", recipe_info["title"])
+
+                # Display ingredients
+                ingredients = recipe_info["extendedIngredients"]
+                st.markdown("Ingredients:")
+                for ingredient in ingredients:
+                    st.write("- " + ingredient["name"])
+
+                # Parse and display summary and instructions as plain text
+                st.markdown("Summary:")
+                summary_html = recipe_info["summary"]
+                summary_text = strip_html_tags(summary_html)
+                st.write(summary_text)
+
+                st.markdown("Instructions:")
+                instructions_html = recipe_info["instructions"]
+                instructions_text = strip_html_tags(instructions_html)
+                st.write(instructions_text)
+            else:
+                st.error(f"API request failed. Status code: {response.status_code}")
+
+
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ("Home", "Recipes", "MYOM", "Ingredients"))
-
-
-
-
+page = st.sidebar.radio("Go to", ("Home", "Recipes", "Ingredients"))
 if page == "Home":
     Home()
 elif page == "Recipes":
     Recipes()
-elif page == "MYOM":
-    MYOM()
+elif page == "Ingredients":
+    Ingredients()
 
 
 
